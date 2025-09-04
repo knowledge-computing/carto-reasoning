ec3886f577d183ccb3971f47740d8d1ed37174c4
Jiyoon Pyo <jiyoonp0228@gmail.com>
2025-09-04 18:14:01 -0500

    add slurm run script

--- cartoreasoning/run.sh
+++ cartoreasoning/run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #SBATCH --job-name=test_model
-#SBATCH --output=/users/2/pyo00005/HOME/MMNIAmapH/output4.out
+#SBATCH --output=output.out
 #SBATCH --time=24:00:00
-#SBATCH -p interactive-gpu
-#SBATCH --gres=gpu:a40:1
+#SBATCH -p preempt-gpu
+#SBATCH --gres=gpu:a40:8
 #SBATCH --ntasks=1
 #SBATCH --mem=100g
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=pyo00005@umn.edu
+#SBATCH --mail-user={EMAIL}
 
 source ~/.bashrc
 
-export HF_HOME=/home/yaoyi/pyo00005/.models
-export HF_HUB_CACHE=/home/yaoyi/pyo00005/.models
+export HF_HOME=./.models
+export HF_HUB_CACHE=./.models
 
 conda init bash
 conda activate testenv
@@ -21,5 +21,5 @@ export CUDA_HOME=$CONDA_PREFIX
 
 nvidia-smi
 
-cd /home/yaoyi/pyo00005/p2/carto-reasoning/cartoreasoning
-python3 run_llava_ov.py --question /home/yaoyi/pyo00005/carto-reasoning/cartoreasoning/responses/mini_runs/response_mini.json --images https://media.githubusercontent.com/media/YOO-uN-ee/carto-image/main/ --flash
+cd ./p2/carto-reasoning/cartoreasoning
+python3 run_llava_ov.py --question ./responses/mini_runs/response_mini.json --images https://media.githubusercontent.com/media/YOO-uN-ee/carto-image/main/ --flash
